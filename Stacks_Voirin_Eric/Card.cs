0abbd5145d9dee1ffc2ce441c1ceabc8ead1660f
SobsForBobs <nontty08@gmail.com>
2026-09-08 08:54:04 -0500

    Initial commit

---
+++ Stacks_Voirin_Eric/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks_Voirin_Eric
+{
+    internal class Card
+    {
+
+        public string Number { get; }
+
+        public string Suit { get; }
+
+        public Card(string numParam, string suitParam)
+        {
+            Number = numParam;
+            Suit = suitParam;
+        }
+
+    }
+}
